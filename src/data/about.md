469535e632c114a9a3744e2f3dfd306e3472ea66
Aditya <adityal@wepay.com>
2023-06-25 17:53:54 -0700

    toning the bio down

--- src/data/about.md
+++ src/data/about.md
@@ -1,15 +1,15 @@
 # Intro
 
 Hi, my name is Aditya Lad.
-I find great joy and fulfillment in the pursuit of knowledge, continuously immersing myself in a vast array of diverse subjects. I am deeply fascinated by ideas and concepts that the world offers and I ardently seek out opportunities to engage in profound and meaningful conversations.
+I find great joy and fulfillment in the pursuit of knowledge, continuously immersing myself in a vast array of diverse subjects.
 
 # Some history
 
 - Growing up I excelled at athletics and I won several medals.
 - At 10, I got my first Intel Pentium 4, 256MB Memory computer.
-- I was obsessed with games namely Age of Empires, Age of Mythology, Need for Speed and Grand Theft Auto.
-- 2016 : Marked a profound turning point in my life as I developed an unwavering devotion to the art of coffee. Since that fateful moment, the aromatic elixir has become an inseparable companion, enhancing my days with its invigorating embrace.
-- 2020: Amidst the tumultuous year of 2020, besieged by the global pandemic, my insatiable appetite for knowledge led me down a captivating path as I delved into the captivating realm of True Crime podcasts. Consuming voraciously, I devoured every episode of Crime Junkie within the span of a mere month, succumbing to the allure of gripping tales shrouded in mystery and intrigue.
+- I was obsessed with games namely Age of Empires, Age of Mythology, Flight Simulator, Need for Speed and Grand Theft Auto.
+- 2016 : As cheesy as it sounds, I developed an unwavering devotion to coffee.
+- 2020: Amidst the tumultuous year of 2020, I got into True Crime, binged watched several TV shows, movies and listened to several podcasts.
 
 # I like
 
@@ -21,7 +21,7 @@ I find great joy and fulfillment in the pursuit of knowledge, continuously immer
 - Running
 - Sketching
 - [Books](https://www.goodreads.com/user/show/36191226-aditya)
-- Podcasts ([The Daily](https://www.nytimes.com/column/the-daily),[Hidden Brain](https://www.npr.org/series/423302056/hidden-brain))
+- Podcasts ([The Daily](https://www.nytimes.com/column/the-daily),[Hidden Brain](https://www.npr.org/series/423302056/hidden-brain), [The Psychology of your 20s](https://www.psych20.com/))
 
 # Travel / Geography
 
--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -32,7 +32,8 @@ const work = [
     highlights: [
       'Developed and implemented highly scalable credit APIs, catering to critical functionalities such as single-use virtual card creation, credit limit settings, and credit summary retrieval. These APIs are currently utilized a staggering 56 million times daily, generating an impressive annual revenue of $1M for the company.',
       'As a vital member of the Credit Squad, exhibited end-to-end ownership of credit products and processes, effectively translating concepts into successful completion.',
-      'Enhanced the quality of testing by implementing the test pyramidal model, resulting in early bug detection and prevention. Additionally, achieved a remarkable 23% increase in the efficiency of deploying binaries within the client validation environment.',
+      'Enhanced the quality of testing by implementing the test pyramidal model, resulting in early bug detection and prevention.',
+      'Additionally, achieved a remarkable 23% increase in the efficiency of deploying binaries within the client validation environment.',
       'Collaborated seamlessly with cross-functional development team members throughout the software development life cycle, actively participating in requirement gathering, design, implementation, and production releases.',
       'Effectively managed and mentored a team of three interns, ensuring their growth, mentorship, and conducting performance evaluations with a high level of responsibility.',
     ],
@@ -59,7 +60,8 @@ const work = [
     endDate: '2018-09-16',
     highlights: [
       'Developed and maintained a robust and scalable distributed systems architecture, ensuring the high availability of the website and internal applications.',
-      'To guarantee optimal performance and reliability, configured, administered, and provided support for the network, storage, operating systems, and load balancers within CapWINs production environment. Successfully mitigated infrastructure risks by migrating bare-metal production servers to VMware EXSI.',
+      'To guarantee optimal performance and reliability, configured, administered, and provided support for the network, storage, operating systems, and load balancers within CapWINs production environment.',
+      'Successfully mitigated infrastructure risks by migrating bare-metal production servers to VMware EXSI.',
       'Exercised proficient administration, support, and monitoring of databases, proactively resolving issues and ensuring the smooth operation of servers.',
       'Implemented methodologies and adhered to best security practices to meet federal requirements, ensuring compliance and safeguarding sensitive data.',
       'Demonstrated a comprehensive understanding of backend networking and excelled in functional programming techniques.',
